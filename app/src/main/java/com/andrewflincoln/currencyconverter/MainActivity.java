a723a7d913ba49d26b3d387c15919e1f5e3e0a89
Andrew Lincoln <andrewjlincoln@gmail.com>
2020-07-28 15:29:33 -0700

    complete

--- app/src/main/java/com/andrewflincoln/currencyconverter/MainActivity.java
+++ app/src/main/java/com/andrewflincoln/currencyconverter/MainActivity.java
@@ -3,8 +3,28 @@ package com.andrewflincoln.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    public void convertFunction(View view) {
+        EditText editTextPounds = (EditText) findViewById(R.id.editTextPounds);
+
+        double pounds = Double.parseDouble(editTextPounds.getText().toString());
+        double dollars = pounds * 1.3;
+
+        Log.i("Info", "Pounds: " + pounds);
+        Log.i("Info", "Dollars: " + dollars);
+
+        Toast.makeText(this, pounds + " pounds = " + dollars + " dollars.", Toast.LENGTH_LONG).show();
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
